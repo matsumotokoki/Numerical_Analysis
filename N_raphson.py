841ceacb02cfdcacb8a679f931eb47c87ee9bdfc
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2018-12-03 17:51:12 +0900

    add graph

--- N_raphson.py
+++ N_raphson.py
@@ -1,20 +1,50 @@
 import math
+from matplotlib import pyplot as plt
 
 EPS  = 0.1**(20)
 flag = False
 ans  = []
+count=0
+
+# def f(x):
+#     return x**3-5*x**2-48*x+108
+# def df(x):
+#     return 3*x**2-10*x-48
+
+# def f(x):
+#     return x**2-2*x-8
+# def df(x):
+#     return 2*x-2
+
+# def f(x):
+#     return x**2+10*x+9
+# def df(x):
+#     return 2*x+10
+
+# def f(x):
+#     return x**3+3*x**2-88*x-180
+# def df(x):
+#     return 3*x**2+6*x-88
 
 def f(x):
-    return x**3-4*x**2+x+6
+    return x**3+4*x**2-31*x-70
 def df(x):
-    return 3*x**2-8*x+1
+    return 3*x**2+8*x-31
 
 RANGE=int(input("how long are search renge? : "))
 STEPS=int(input("how long are search steps? : "))
+# RANGE = 100
+# STEPS = 3
 for x in range(-RANGE,RANGE,STEPS):
+    c=0
+    x_axis = []
+    x_plot = []
     while True:
         if df(x)!=0:
             x -=f(x)/df(x)
+            x_axis.append(c)
+            x_plot.append(x)
+            c+=1
         else:
             flag=True
             break
@@ -29,4 +59,7 @@ for x in range(-RANGE,RANGE,STEPS):
         if flag==False:
             ans.append(x)
         flag=False
+    plt.plot(x_axis,x_plot)
+
 print(ans)
+plt.show()

e3760a0ad5fc987b6e904ea9a64ed7d8d9f01f61
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2018-12-04 17:17:50 +0900

    print

--- N_raphson.py
+++ N_raphson.py
@@ -6,10 +6,15 @@ flag = False
 ans  = []
 count=0
 
+def f(x):
+    return x**3-5*x**2-48*x+108
+def df(x):
+    return 3*x**2-10*x-48
+
 # def f(x):
-#     return x**3-5*x**2-48*x+108
+#     return x**3+x**2
 # def df(x):
-#     return 3*x**2-10*x-48
+#     return 3*x**2+2*x
 
 # def f(x):
 #     return x**2-2*x-8
@@ -26,22 +31,24 @@ count=0
 # def df(x):
 #     return 3*x**2+6*x-88
 
-def f(x):
-    return x**3+4*x**2-31*x-70
-def df(x):
-    return 3*x**2+8*x-31
+# def f(x):
+#     return x**3+4*x**2-31*x-70
+# def df(x):
+#     return 3*x**2+8*x-31
 
 RANGE=int(input("how long are search renge? : "))
 STEPS=int(input("how long are search steps? : "))
 # RANGE = 100
 # STEPS = 3
 for x in range(-RANGE,RANGE,STEPS):
+    print("start :"+str(x))
     c=0
     x_axis = []
     x_plot = []
     while True:
         if df(x)!=0:
             x -=f(x)/df(x)
+            print ("---round---"+str(c+1)+" : "+str(x))
             x_axis.append(c)
             x_plot.append(x)
             c+=1
@@ -60,6 +67,7 @@ for x in range(-RANGE,RANGE,STEPS):
             ans.append(x)
         flag=False
     plt.plot(x_axis,x_plot)
+    print()
 
-print(ans)
+print ("finish\nanser is "+str(ans))
 plt.show()
